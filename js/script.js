3065e7bf033a835a7b9fc7e143a74a9911088805
Dueel <diego.bar@live.it>
2021-03-22 15:14:29 +0100

    Esercizio completo

--- js/script.js
+++ js/script.js
@@ -2,18 +2,30 @@ function prevImg(){
 
     var currentImg = $('.active');
     var prevImg = currentImg.prev();
-
     currentImg.removeClass('active');
-    prevImg.addClass('active')
+
+    if (currentImg.hasClass('first')){
+        
+        $('.last').addClass('active')
+    } else {
+        
+        prevImg.addClass('active');
+    }
 };
 
 function nextImg(){
 
     var currentImg = $('.active');
     var nextImg = currentImg.next();
-
     currentImg.removeClass('active');
-    nextImg.addClass('active')
+
+    if (currentImg.hasClass('last')){
+        
+        $('.first').addClass('active');
+    } else {
+        
+        nextImg.addClass('active');
+    }
 };
 
 function init(){
