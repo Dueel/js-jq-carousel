845b11c955e06ec2f132bd8bdf0488decc37094e
Dueel <diego.bar@live.it>
2021-03-22 15:15:59 +0100

    Pulizia codice

--- js/script.js
+++ js/script.js
@@ -35,12 +35,3 @@ function init(){
 };
 
 $('document').ready(init);
-
-
-// - associare il click alle due freccie; - 
-
-// - selezionare l'immagine attuale -
-
-// - al click, togliere la classe .active all' img attuale e associarla a quella successiva con .next() - 
-
-// - far ripartire il giro con una codizione (associare altre due classi: first, last)
